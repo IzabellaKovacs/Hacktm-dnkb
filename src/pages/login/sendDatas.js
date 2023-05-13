b31549eb1cb1965d8544d2a8d1b449a2a3959e6e
Ionut Nahoi <ioan.nahoi02@e-uvt.ro>
2023-05-13 06:05:22 +0300

    make to bring accounts to the main page

--- src/pages/login/sendDatas.js
+++ src/pages/login/sendDatas.js
@@ -1,19 +1,13 @@
 import { useEffect } from "react";
 export default function sendDatas(mail, password) {
- console.log(mail, password);
-  fetch(
-    `http://localhost:8081/User/validation/${mail}/${password}`,
-    {
-      method: "GET",
-    }
-  ).then((res) => {
+  console.log(mail, password);
+  fetch(`http://localhost:8081/User/validation/${mail}/${password}`, {
+    method: "GET",
+  }).then((res) => {
     if (res.status === 200) {
       //   alert("ok");
       console.log(res.status);
-    //   res.json().then((data) => {
-    //     localStorage.setItem("user", JSON.stringify(data));
-    //   });
-    localStorage.setItem("mail", mail);
+      localStorage.setItem("mail", mail);
       window.location.href = "/MainPage";
     } else {
       alert("wrong data!");
--- src/pages/mainPage/mainPage.js
+++ src/pages/mainPage/mainPage.js
@@ -4,39 +4,35 @@ import Card from "../../components/card";
 import NavigationBar from "../../components/navBar";
 import { useEffect } from "react";
 
-const products = [
-  { title: 'Cabbage', id: 1 },
-  { title: 'Garlic', id: 2 },
-  { title: 'Apple', id: 3 },
-  { title: 'samsung', id: 4 },
-  { title: 'xiaomi', id: 5 },
-];
+// const products = [
+//   { title: 'Cabbage', id: 1 },
+//   { title: 'Garlic', id: 2 },
+//   { title: 'Apple', id: 3 },
+//   { title: 'samsung', id: 4 },
+//   { title: 'xiaomi', id: 5 },
+// ];
 
 function MainPage() {
-  // const [products, setProducts] = React.useState([]);
-  const [user, setUser] = React.useState("");
+  const [products, setProducts] = React.useState([]);
+  const [mail, setMail] = React.useState(localStorage.getItem("mail"));
 
-  // useEffect(() => {
-  //   const items = JSON.parse(localStorage.getItem("mail"));
-  //   if (items) {
-  //     setUser(items);
-  //   }
-  // }, []);
+  
 
-  // useEffect(() => {
-  //   fetch(`http://localhost:8080/Accounts/login?${user}`)
-  //     .then((res) => res.json())
-  //     .then((result) => {
-  //       setProducts(result);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch(`http://localhost:8081/Access/access_role/${mail}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setProducts(result);
+        console.log(result);
+      });
+  }, []);
   
   return (
     <div className="container">
       <NavigationBar />
       <div className="card--elements">
         {products.map((item) => (
-          <Card key={item.id} name={item.title} />
+          <Card key={item.id} name={item.account. name} />
         ))}
       </div>
     </div>
